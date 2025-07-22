8672b829dc1eff517089cadbfa8af136b2e63132
sarangerin <sarang.erin@gmail.com>
2025-07-22 01:16:00 +0000

    bug fix

--- src/download_ppi_data.py
+++ src/download_ppi_data.py
@@ -1,7 +1,7 @@
 import json
 
 from cellmaps_ppidownloader.gene import (APMSGeneNodeAttributeGenerator,
-                                         CM4AIGeneNodeAttributeGenerator)
+                                         NdexGeneNodeAttributeGenerator)
 from cellmaps_ppidownloader.runner import CellmapsPPIDownloader
 
 
@@ -9,16 +9,25 @@ json_prov = None
 with open("examples/provenance.json", 'r') as f:
     json_prov = json.load(f)
 
+#apmsgen = APMSGeneNodeAttributeGenerator(
+#    apms_edgelist=APMSGeneNodeAttributeGenerator.get_apms_edgelist_from_tsvfile("examples/edgelist.tsv",
+#                                                                                geneid_one_col=APMSGeneNodeAttributeGenerator.GENEID_COL1,
+#                                                                                symbol_one_col=APMSGeneNodeAttributeGenerator.SYMBOL_COL1,
+#                                                                                geneid_two_col=APMSGeneNodeAttributeGenerator.GENEID_COL2,
+#                                                                                symbol_two_col=APMSGeneNodeAttributeGenerator.SYMBOL_COL2),
+#    apms_baitlist=APMSGeneNodeAttributeGenerator.get_apms_baitlist_from_tsvfile("examples/baitlist.tsv",
+#                                                                                symbol_col=APMSGeneNodeAttributeGenerator.BAITLIST_GENE_SYMBOL,
+#                                                                                geneid_col=APMSGeneNodeAttributeGenerator.BAITLIST_GENE_ID,
+#                                                                                numinteractors_col=APMSGeneNodeAttributeGenerator.BAITLIST_NUM_INTERACTORS))
+
+UUID = "95bc75d5-d1d1-11ee-8a40-005056ae23aa"
+
+ndexgen = NdexGeneNodeAttributeGenerator(uuid=UUID)
+
 apmsgen = APMSGeneNodeAttributeGenerator(
-    apms_edgelist=APMSGeneNodeAttributeGenerator.get_apms_edgelist_from_tsvfile("examples/edgelist.tsv",
-                                                                                geneid_one_col=APMSGeneNodeAttributeGenerator.GENEID_COL1,
-                                                                                symbol_one_col=APMSGeneNodeAttributeGenerator.SYMBOL_COL1,
-                                                                                geneid_two_col=APMSGeneNodeAttributeGenerator.GENEID_COL2,
-                                                                                symbol_two_col=APMSGeneNodeAttributeGenerator.SYMBOL_COL2),
-    apms_baitlist=APMSGeneNodeAttributeGenerator.get_apms_baitlist_from_tsvfile("examples/baitlist.tsv",
-                                                                                symbol_col=APMSGeneNodeAttributeGenerator.BAITLIST_GENE_SYMBOL,
-                                                                                geneid_col=APMSGeneNodeAttributeGenerator.BAITLIST_GENE_ID,
-                                                                                numinteractors_col=APMSGeneNodeAttributeGenerator.BAITLIST_NUM_INTERACTORS))
+    apms_edgelist=ndexgen.get_apms_edgelist(ndexgen.nice_cx, ndexgen.node_attrs_list),
+    apms_baitlist=ndexgen.get_apms_baitlist_from_ndex(ndexgen.nice_cx)
+)
 
 ppi_downloader = CellmapsPPIDownloader(
     outdir="data/ppi",
